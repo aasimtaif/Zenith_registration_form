4ad42ecbb50e0a1c34777c57ceeae3b5199731ad
Aasim Taif <taifullahansari@gmail.com>
2023-03-09 07:35:40 +0530

    updated the mongoose schema

--- backend/models/data.model.js
+++ backend/models/data.model.js
@@ -1,7 +1,14 @@
 const mongoose = require('mongoose')
 const teamSchema = new mongoose.Schema({
-    collegename:String,
+    college_name:String,
     sport:String,
+    sportssecretary_name:String,
+    sportssecretary_Phone_no:Number,
+    sportssecretary_email:String,
+    captains_Phone_no:Number,
+    captains_name:String,
+    captains_email:String,
+    college_location:String,
 })
 
 const teamModel = mongoose.model('Team', teamSchema)
